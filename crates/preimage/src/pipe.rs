512031f43e9bebfd1a2414f579b5812141867c31
clabby <ben@clab.by>
2024-06-02 00:52:42 -0400

    feat(preimage): Async client handles
    Makes the `HintWriterClient` + `PreimageOracleClient` traits
    asynchronous to prevent blocking of the host program when executing a
    client program natively.
    
    Previously, since the preimage oracle bindings for the client were
    entirely synchronous, the loops in `PipeHandle` could cause a deadlock.
    Now that oracle IO is asynchronous, the runtime can interrupt a future
    when it yields execution (i.e. `tokio::select` works.)
    
    In the client program, synchronous execution is still guaranteed. It can
    run async colored functions in a minimal runtime, such as the `block_on`
    runtime in `kona_common`. `simple-revm` had to be changed as a part of
    this PR, which has an example of this.

--- crates/preimage/src/pipe.rs
+++ crates/preimage/src/pipe.rs
@@ -1,7 +1,14 @@
 //! This module contains a rudamentary pipe between two file descriptors, using [kona_common::io]
 //! for reading and writing from the file descriptors.
 
-use anyhow::{bail, Result};
+use anyhow::{anyhow, Result};
+use core::{
+    cell::RefCell,
+    cmp::Ordering,
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
 use kona_common::{io, FileDescriptor};
 
 /// [PipeHandle] is a handle for one end of a bidirectional pipe.
@@ -24,30 +31,14 @@ impl PipeHandle {
         io::read(self.read_handle, buf)
     }
 
-    /// Reads exactly `buf.len()` bytes into `buf`, blocking until all bytes are read.
-    pub fn read_exact(&self, buf: &mut [u8]) -> Result<usize> {
-        let mut read = 0;
-        while read < buf.len() {
-            let chunk_read = self.read(&mut buf[read..])?;
-            read += chunk_read;
-        }
-        Ok(read)
+    /// Reads exactly `buf.len()` bytes into `buf`.
+    pub fn read_exact<'a>(&self, buf: &'a mut [u8]) -> impl Future<Output = Result<usize>> + 'a {
+        ReadFuture { pipe_handle: *self, buf: RefCell::new(buf), read: 0 }
     }
 
     /// Write the given buffer to the pipe.
-    pub fn write(&self, buf: &[u8]) -> Result<usize> {
-        let mut written = 0;
-        loop {
-            match io::write(self.write_handle, &buf[written..]) {
-                Ok(0) => break,
-                Ok(n) => {
-                    written += n;
-                    continue;
-                }
-                Err(e) => bail!("Failed to write preimage key: {}", e),
-            }
-        }
-        Ok(written)
+    pub fn write<'a>(&self, buf: &'a [u8]) -> impl Future<Output = Result<usize>> + 'a {
+        WriteFuture { pipe_handle: *self, buf, written: 0 }
     }
 
     /// Returns the read handle for the pipe.
@@ -58,5 +49,67 @@ impl PipeHandle {
     /// Returns the write handle for the pipe.
     pub fn write_handle(&self) -> FileDescriptor {
         self.write_handle
+    }
+}
+
+/// A future that reads from a pipe, returning [Poll::Ready] when the buffer is full.
+struct ReadFuture<'a> {
+    /// The pipe handle to read from
+    pipe_handle: PipeHandle,
+    /// The buffer to read into
+    buf: RefCell<&'a mut [u8]>,
+    /// The number of bytes read so far
+    read: usize,
+}
+
+impl Future for ReadFuture<'_> {
+    type Output = Result<usize>;
+
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut buf = self.buf.borrow_mut();
+        let buf_len = buf.len();
+        let chunk_read = self.pipe_handle.read(&mut buf[self.read..])?;
+
+        // Drop the borrow on self.
+        drop(buf);
+
+        self.read += chunk_read;
+
+        match self.read.cmp(&buf_len) {
+            Ordering::Equal => Poll::Ready(Ok(self.read)),
+            Ordering::Greater => Poll::Ready(Err(anyhow!("Read more bytes than buffer size"))),
+            Ordering::Less => {
+                // Register the current task to be woken up when it can make progress
+                ctx.waker().wake_by_ref();
+                Poll::Pending
+            }
+        }
+    }
+}
+
+/// A future that writes to a pipe, returning [Poll::Ready] when the full buffer has been written.
+struct WriteFuture<'a> {
+    /// The pipe handle to write to
+    pipe_handle: PipeHandle,
+    /// The buffer to write
+    buf: &'a [u8],
+    /// The number of bytes written so far
+    written: usize,
+}
+
+impl Future for WriteFuture<'_> {
+    type Output = Result<usize>;
+
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+        match io::write(self.pipe_handle.write_handle(), &self.buf[self.written..]) {
+            Ok(0) => Poll::Ready(Ok(self.written)), // Finished writing
+            Ok(n) => {
+                self.written += n;
+                // Register the current task to be woken up when it can make progress
+                ctx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            Err(e) => Poll::Ready(Err(e)),
+        }
     }
 }
