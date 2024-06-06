5f3a8096c49218cee3eb00d17a1908b9ce13ccfe
refcell <abigger87@gmail.com>
2024-06-06 12:19:16 -0600

    fix: use online provider impls

--- crates/derive/src/pipeline/builder.rs
+++ crates/derive/src/pipeline/builder.rs
@@ -23,22 +23,70 @@ type AttributesQueueStage<DAP, P, T, B> = AttributesQueue<BatchQueueStage<DAP, P
 ///
 /// ## Usage
 ///
-/// ```rust,ignore
+/// ```rust
+/// #![cfg(feature = "online")]
+///
 /// use alloc::sync::Arc;
-/// use kona_derive::pipeline::*;
+/// use alloy_provider::ReqwestProvider;
+/// use alloy_rpc_client::RpcClient;
+/// use alloy_transport_http::Http;
+/// use kona_derive::{
+///     online::{
+///         AlloyChainProvider, AlloyL2ChainProvider, OnlineBeaconClient, OnlineBlobProvider,
+///         SimpleSlotDerivation,
+///     },
+///     pipeline::*,
+/// };
+/// use reqwest::Client;
 ///
-/// // TODO(refcell): replace this will a rollup config
-/// // fetched from the superchain-registry via network id.
-/// let rollup_config = Arc::new(RollupConfig::default());
+/// #[allow(clippy::needless_doctest_main)]
+/// fn main() {
+///     // Creates a new chain provider using the `L1_RPC_URL` environment variable.
+///     let l1_rpc_url = std::env::var("L1_RPC_URL").expect("L1_RPC_URL must be set");
+///     let l1_rpc_url = l1_rpc_url.parse().unwrap();
+///     let http = Http::<Client>::new(l1_rpc_url);
+///     let chain_provider =
+///         AlloyChainProvider::new(ReqwestProvider::new(RpcClient::new(http, true)));
 ///
-/// // Build the ethereum data source
-/// let chain_provider = ExExChainProvider::new(Arc::clone(&self.chain_provider));
-/// let blob_provider = ExExBlobProvider::new(Arc::clone(&self.blob_provider));
-/// let dap_source = EthereumDataSource::new(chain_provider, blob_provider, &rollup_config);
+///     // Creates a new l2 chain provider using the `L2_RPC_URL` environment variable.
+///     let l2_rpc_url = std::env::var("L2_RPC_URL").expect("L2_RPC_URL must be set");
+///     let l2_rpc_url = l2_rpc_url.parse().unwrap();
+///     let http = Http::<Client>::new(l2_rpc_url);
+///     let l2_chain_provider =
+///         AlloyL2ChainProvider::new(ReqwestProvider::new(RpcClient::new(http, true)));
 ///
-/// let builder = PipelineBuilder::new();
-/// let pipeline = builder
-///    .rollup_config(rollup_config)
+///     // TODO(refcell): replace this will a rollup config
+///     // fetched from the superchain-registry via network id.
+///     let rollup_config = Arc::new(RollupConfig::default());
+///
+///     // Create the beacon client used to fetch blob data.
+///     let beacon_url = std::env::var("BEACON_URL").expect("BEACON_URL must be set");
+///     let beacon_url = beacon_url.parse().unwrap();
+///     let http = Http::<Client>::new(beacon_url);
+///     let beacon_client =
+///         OnlineBeaconClient::new(ReqwestProvider::new(RpcClient::new(http, true)));
+///
+///     // Build the online blob provider.
+///     let blob_provider: OnlineBlobProvider<_, SimpleSlotDerivation> =
+///         OnlineBlobProvider::new(true, beacon_client, None, None);
+///
+///     // Build the ethereum data source
+///     let dap_source =
+///         EthereumDataSource::new(chain_provider.clone(), blob_provider, &rollup_config);
+///
+///     let builder = PipelineBuilder::new();
+///     let pipeline = builder
+///         .rollup_config(rollup_config)
+///         .dap_source(dap_source)
+///         .l2_chain_provider(l2_chain_provider)
+///         .chain_provider(chain_provider)
+///         .builder(OnlineAttributesBuilder::new())
+///         .reset(ResetProvider::new())
+///         .start_cursor(L2BlockInfo::default())
+///         .build();
+///
+///     assert_eq!(pipeline.needs_reset, false);
+/// }
 /// ```
 #[derive(Debug)]
 pub struct PipelineBuilder<R, B, P, T, D>
