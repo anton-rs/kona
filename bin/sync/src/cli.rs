f34748089d8a942241c6f7b43d39c02b0c6ebfb2
refcell <abigger87@gmail.com>
2024-06-12 16:53:32 -0500

    fix: bin cleanup

---
+++ bin/sync/src/cli.rs
@@ -0,0 +1,23 @@
+//! This module contains all CLI-specific code.
+
+use clap::{ArgAction, Parser};
+
+/// The host binary CLI application arguments.
+#[derive(Parser, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Cli {
+    /// Verbosity level (0-4)
+    #[arg(long, short, help = "Verbosity level (0-4)", action = ArgAction::Count)]
+    pub v: u8,
+    /// The l1 rpc URL
+    #[clap(long)]
+    pub l1_rpc_url: Option<String>,
+    /// The l2 rpc URL
+    #[clap(long)]
+    pub l2_rpc_url: Option<String>,
+    /// The Beacon URL
+    #[clap(long)]
+    pub beacon_url: Option<String>,
+    /// The l2 block to start from.
+    #[clap(long, short, help = "Starting l2 block, defaults to chain genesis if none specified")]
+    pub start_l2_block: Option<u64>,
+}
