b2b000b75b62cbaf0feca3af94cccd6c7ff4efe5
Zach Obront <zobront@gmail.com>
2024-06-24 15:41:57 -0500

    add zkvm io

---
+++ crates/common/src/zkvm/mod.rs
@@ -0,0 +1,4 @@
+//! This module contains raw syscall bindings for the `riscv64gc` target architecture, as well as a
+//! high-level implementation of the [crate::BasicKernelInterface] trait for the `asterisc` kernel.
+
+pub(crate) mod io;
