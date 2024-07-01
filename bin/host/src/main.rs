7c1486fbfcf99a26d7b781b17e75c4e35aec26bf
clabby <ben@clab.by>
2024-06-30 20:17:51 -0400

    feat(ci): Run Host + Client natively in offline mode
    Adds a few jobs to CI to run the host + client program against known
    witness data in offline mode, over both the Ecotone and Fjord hardfork.

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -60,26 +60,27 @@ async fn start_server(cfg: HostCli) -> Result<()> {
 
     let kv_store = cfg.construct_kv_store();
 
-    let beacon_client = OnlineBeaconClient::new_http(
-        cfg.l1_beacon_address.clone().expect("Beacon API URL must be set"),
-    );
-    let mut blob_provider = OnlineBlobProvider::new(beacon_client, None, None);
-    blob_provider
-        .load_configs()
-        .await
-        .map_err(|e| anyhow!("Failed to load blob provider configuration: {e}"))?;
-
-    let fetcher = (!cfg.is_offline()).then(|| {
+    let fetcher = if !cfg.is_offline() {
+        let beacon_client = OnlineBeaconClient::new_http(
+            cfg.l1_beacon_address.clone().expect("Beacon API URL must be set"),
+        );
+        let mut blob_provider = OnlineBlobProvider::new(beacon_client, None, None);
+        blob_provider
+            .load_configs()
+            .await
+            .map_err(|e| anyhow!("Failed to load blob provider configuration: {e}"))?;
         let l1_provider = util::http_provider(&cfg.l1_node_address.expect("Provider must be set"));
         let l2_provider = util::http_provider(&cfg.l2_node_address.expect("Provider must be set"));
-        Arc::new(RwLock::new(Fetcher::new(
+        Some(Arc::new(RwLock::new(Fetcher::new(
             kv_store.clone(),
             l1_provider,
             blob_provider,
             l2_provider,
             cfg.l2_head,
-        )))
-    });
+        ))))
+    } else {
+        None
+    };
 
     // Start the server and wait for it to complete.
     info!("Starting preimage server.");
@@ -96,28 +97,29 @@ async fn start_server_and_native_client(cfg: HostCli) -> Result<()> {
     let (preimage_pipe, hint_pipe, files) = util::create_native_pipes()?;
     let kv_store = cfg.construct_kv_store();
 
-    let beacon_client = OnlineBeaconClient::new_http(
-        cfg.l1_beacon_address.clone().expect("Beacon API URL must be set"),
-    );
-    let mut blob_provider = OnlineBlobProvider::new(beacon_client, None, None);
-    blob_provider
-        .load_configs()
-        .await
-        .map_err(|e| anyhow!("Failed to load blob provider configuration: {e}"))?;
-
-    let fetcher = (!cfg.is_offline()).then(|| {
+    let fetcher = if !cfg.is_offline() {
+        let beacon_client = OnlineBeaconClient::new_http(
+            cfg.l1_beacon_address.clone().expect("Beacon API URL must be set"),
+        );
+        let mut blob_provider = OnlineBlobProvider::new(beacon_client, None, None);
+        blob_provider
+            .load_configs()
+            .await
+            .map_err(|e| anyhow!("Failed to load blob provider configuration: {e}"))?;
         let l1_provider =
             util::http_provider(cfg.l1_node_address.as_ref().expect("Provider must be set"));
         let l2_provider =
             util::http_provider(cfg.l2_node_address.as_ref().expect("Provider must be set"));
-        Arc::new(RwLock::new(Fetcher::new(
+        Some(Arc::new(RwLock::new(Fetcher::new(
             kv_store.clone(),
             l1_provider,
             blob_provider,
             l2_provider,
             cfg.l2_head,
-        )))
-    });
+        ))))
+    } else {
+        None
+    };
 
     // Create the server and start it.
     let server_task =
