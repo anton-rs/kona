79300b7c36b402747aa7e403cfa0592aab688957
refcell <abigger87@gmail.com>
2024-07-11 09:02:40 -0400

    fix(examples): trusted sync dockerfile

--- examples/trusted-sync/run.sh
+++ examples/trusted-sync/run.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/bin/sh
 
 START="${START_L2_BLOCK:-0}"
 METRICS="${METRICS_URL:-0.0.0.0:9000}"
 
 # If the `START_BLOCKS_FROM_TIP` environment variable is set, we will start syncing from the tip of the chain.
 if [ -n "$START_BLOCKS_FROM_TIP" ]; then
-  /usr/local/bin/trusted-sync \
+  /bin/trusted-sync \
     --l1-rpc-url $L1_RPC_URL \
     --l2-rpc-url $L2_RPC_URL \
     --beacon-url $BEACON_URL \
@@ -13,7 +13,7 @@ if [ -n "$START_BLOCKS_FROM_TIP" ]; then
     --start-blocks-from-tip $START_BLOCKS_FROM_TIP \
     -vvv
 else
-  /usr/local/bin/trusted-sync \
+  /bin/trusted-sync \
     --l1-rpc-url $L1_RPC_URL \
     --l2-rpc-url $L2_RPC_URL \
     --beacon-url $BEACON_URL \
