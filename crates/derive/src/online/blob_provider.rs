e4a65ae4f91e8b255f56ead7f904ea8504d8ad8f
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-07-31 23:50:29 +0200

    feat(derive): online blob provider with fallback

--- crates/derive/src/online/blob_provider.rs
+++ crates/derive/src/online/blob_provider.rs
@@ -176,6 +176,161 @@ where
     }
 }
 
+/// The minimal interface required to fetch sidecars from a remote blob store.
+#[async_trait]
+pub trait BlobSidecarProvider {
+    /// Fetches blob sidecars that were confirmed in the specified L1 block with the given indexed
+    /// hashes. Order of the returned sidecars is guaranteed to be that of the hashes. Blob data is
+    /// not checked for validity.
+    ///
+    /// Consensus specs: <https://ethereum.github.io/beacon-APIs/#/Beacon/getBlobSidecars>
+    async fn beacon_blob_side_cars(
+        &self,
+        slot: u64,
+        hashes: &[IndexedBlobHash],
+    ) -> anyhow::Result<Vec<APIBlobSidecar>>;
+}
+
+/// Blanket implementation of the [BlobSidecarProvider] trait for all types that
+/// implemend [BeaconClient], which has a superset of the required functionality.
+#[async_trait]
+impl<B: BeaconClient + Send + Sync> BlobSidecarProvider for B {
+    async fn beacon_blob_side_cars(
+        &self,
+        slot: u64,
+        hashes: &[IndexedBlobHash],
+    ) -> anyhow::Result<Vec<APIBlobSidecar>> {
+        self.beacon_blob_side_cars(slot, hashes).await
+    }
+}
+
+/// An online blob provider that optionally falls back to a secondary provider if the
+/// primary fails to fetch blob sidecars.
+///
+/// This is useful for scenarios where blobs have been evicted from the primary provider's
+/// blob store and need to be fetched from a remote archive API. The default eviction
+/// policy on Ethereum is to keep blobs for 18 days.
+///
+/// Blob storage APIs are expected to implement the [BlobSidecarProvider] trait.
+/// One example can be found at <https://github.com/base-org/blob-archiver>
+#[derive(Debug, Clone)]
+pub struct OnlineBlobProviderWithFallback<
+    B: BeaconClient,
+    F: BlobSidecarProvider,
+    S: SlotDerivation,
+> {
+    primary: OnlineBlobProvider<B, S>,
+    fallback: F,
+    _slot_derivation: PhantomData<S>,
+}
+
+impl<B: BeaconClient, F: BlobSidecarProvider, S: SlotDerivation>
+    OnlineBlobProviderWithFallback<B, F, S>
+{
+    /// Creates a new instance of the [OnlineBlobProviderWithFallback] with the
+    /// specified primary and fallback providers.
+    pub fn new(primary: OnlineBlobProvider<B, S>, fallback: F) -> Self {
+        Self { primary, fallback, _slot_derivation: PhantomData }
+    }
+
+    /// Attempts to fetch blob sidecars from the fallback provider.
+    async fn fallback_fetch_filtered_sidecars(
+        &self,
+        block_ref: &BlockInfo,
+        blob_hashes: &[IndexedBlobHash],
+    ) -> Result<Vec<BlobSidecar>, BlobProviderError> {
+        if blob_hashes.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        // Fetch blob sidecars for the given block reference and blob hashes.
+        let sidecars =
+            self.fallback.beacon_blob_side_cars(block_ref.timestamp, blob_hashes).await?;
+
+        // Filter blob sidecars that match the indicies in the specified list.
+        let blob_hash_indicies = blob_hashes.iter().map(|b| b.index).collect::<Vec<_>>();
+        let filtered = sidecars
+            .into_iter()
+            .filter(|s| blob_hash_indicies.contains(&(s.inner.index as usize)))
+            .collect::<Vec<_>>();
+
+        // Validate the correct number of blob sidecars were retrieved.
+        if blob_hashes.len() != filtered.len() {
+            return Err(BlobProviderError::SidecarLengthMismatch(blob_hashes.len(), filtered.len()));
+        }
+
+        Ok(filtered.into_iter().map(|s| s.inner).collect::<Vec<BlobSidecar>>())
+    }
+}
+
+#[async_trait]
+impl<B, F, S> BlobProvider for OnlineBlobProviderWithFallback<B, F, S>
+where
+    B: BeaconClient + Send + Sync,
+    F: BlobSidecarProvider + Send + Sync,
+    S: SlotDerivation + Send + Sync,
+{
+    /// Fetches blob sidecars that were confirmed in the specified L1 block with the given indexed
+    /// hashes. The blobs are validated for their index and hashes using the specified
+    /// [IndexedBlobHash].
+    async fn get_blobs(
+        &mut self,
+        block_ref: &BlockInfo,
+        blob_hashes: &[IndexedBlobHash],
+    ) -> Result<Vec<Blob>, BlobProviderError> {
+        crate::inc!(PROVIDER_CALLS, &["blob_provider", "get_blobs"]);
+        crate::timer!(START, PROVIDER_RESPONSE_TIME, &["blob_provider", "get_blobs"], timer);
+        match self.primary.get_blobs(block_ref, blob_hashes).await {
+            Ok(blobs) => Ok(blobs),
+            Err(_primary_err) => {
+                crate::inc!(PROVIDER_ERRORS, &["blob_provider", "get_blobs", "primary"]);
+
+                // Fetch the blob sidecars for the given block reference and blob hashes.
+                let sidecars =
+                    match self.fallback_fetch_filtered_sidecars(block_ref, blob_hashes).await {
+                        Ok(sidecars) => sidecars,
+                        Err(e) => {
+                            crate::timer!(DISCARD, timer);
+                            crate::inc!(
+                                PROVIDER_ERRORS,
+                                &["blob_provider", "get_blobs", "fallback_fetch_filtered_sidecars"]
+                            );
+                            return Err(e);
+                        }
+                    };
+
+                // Validate the blob sidecars straight away with the `IndexedBlobHash`es.
+                let blobs = match sidecars
+                    .into_iter()
+                    .enumerate()
+                    .map(|(i, sidecar)| {
+                        let hash = blob_hashes
+                            .get(i)
+                            .ok_or(anyhow!("fallback: failed to get blob hash"))?;
+                        match sidecar.verify_blob(hash) {
+                            Ok(_) => Ok(sidecar.blob),
+                            Err(e) => Err(e),
+                        }
+                    })
+                    .collect::<anyhow::Result<Vec<Blob>>>()
+                {
+                    Ok(blobs) => blobs,
+                    Err(e) => {
+                        crate::timer!(DISCARD, timer);
+                        crate::inc!(
+                            PROVIDER_ERRORS,
+                            &["blob_provider", "get_blobs", "fallback_verify_blob"]
+                        );
+                        return Err(BlobProviderError::Custom(e));
+                    }
+                };
+
+                Ok(blobs)
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -431,5 +586,55 @@ mod tests {
             result.unwrap_err(),
             BlobProviderError::Custom(anyhow::anyhow!("blob at index 0 failed verification"))
         );
+    }
+
+    #[tokio::test]
+    async fn test_get_blobs_fallback() {
+        let json_bytes = include_bytes!("testdata/eth_v1_beacon_sidecars_goerli.json");
+        let all_sidecars: APIGetBlobSidecarsResponse = serde_json::from_slice(json_bytes).unwrap();
+
+        let online_sidecars = APIGetBlobSidecarsResponse {
+            // Remove some sidecars from the online provider to trigger a fallback fetch
+            data: all_sidecars.data.clone().into_iter().take(2).collect::<Vec<_>>(),
+        };
+
+        let beacon_client = MockBeaconClient {
+            beacon_genesis: Some(APIGenesisResponse::new(10)),
+            config_spec: Some(APIConfigResponse::new(12)),
+            blob_sidecars: Some(online_sidecars),
+            ..Default::default()
+        };
+        let fallback_client =
+            MockBeaconClient { blob_sidecars: Some(all_sidecars), ..Default::default() };
+        let mut blob_provider: OnlineBlobProviderWithFallback<_, _, SimpleSlotDerivation> =
+            OnlineBlobProviderWithFallback::new(
+                OnlineBlobProvider::new(beacon_client, None, None),
+                fallback_client,
+            );
+        let block_ref = BlockInfo { timestamp: 15, ..Default::default() };
+        let blob_hashes = vec![
+            IndexedBlobHash {
+                index: 0,
+                hash: b256!("011075cbb20f3235b3179a5dff22689c410cd091692180f4b6a12be77ea0f586"),
+            },
+            IndexedBlobHash {
+                index: 1,
+                hash: b256!("010a9e10aab79bab62e10a5b83c164a91451b6ef56d31ac95a9514ffe6d6b4e6"),
+            },
+            IndexedBlobHash {
+                index: 2,
+                hash: b256!("016122c8e41c69917b688240707d107aa6d2a480343e4e323e564241769a6b4a"),
+            },
+            IndexedBlobHash {
+                index: 3,
+                hash: b256!("01df1f9ae707f5847513c9c430b683182079edf2b1f94ee12e4daae7f3c8c309"),
+            },
+            IndexedBlobHash {
+                index: 4,
+                hash: b256!("01e5ee2f6cbbafb3c03f05f340e795fe5b5a8edbcc9ac3fc7bd3d1940b99ef3c"),
+            },
+        ];
+        let blobs = blob_provider.get_blobs(&block_ref, &blob_hashes).await.unwrap();
+        assert_eq!(blobs.len(), 5);
     }
 }
