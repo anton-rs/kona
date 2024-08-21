ceef403147834e89d8c1378e9749ed508e449cbf
refcell <abigger87@gmail.com>
2024-08-21 14:46:36 -0400

    feat(kdn): Derivation Test Runner for kona-derive (#414)
    * feat(kd8n): derivation test runner for kona-derive
    
    * stash
    
    * chore: kd8n
    
    * fix: providers
    
    * fix: it runs but missing l2 blocks for validation
    
    * fix: kdn
    
    * fix: kdn
    
    * feat: kdn works
    
    * fix: point to upstream op-test-vectors
    
    * fixes
    
    * fix(kdn): use pinned kona derive re-export in op-test-vectors
    
    * chore(kdn): print results to terminal in happy path
    
    * fix(kdn): op-test-vectors submodule
    
    * fixes
    
    * fix(kdn): op-test-vectors submodule
    
    * fixes
    
    * fix(kdn): op-test-vectors submodule
    
    * fix(kdn): formatting
    
    * fix(ci): checkout submodules
    
    * fix(ci): checkout submodules
    
    * fix(ci): dont checkout submodules recursive

---
+++ bin/kdn/src/cli.rs
@@ -0,0 +1,130 @@
+//! Module for the CLI.
+
+use anyhow::{anyhow, Result};
+use clap::{ArgAction, Parser};
+use include_directory::{include_directory, Dir, DirEntry, File};
+use op_test_vectors::derivation::DerivationFixture;
+use tracing::{debug, error, info, trace, warn, Level};
+
+static OP_TEST_VECTORS: Dir<'_> =
+    include_directory!("$CARGO_MANIFEST_DIR/op-test-vectors/fixtures/derivation/");
+
+/// Main CLI
+#[derive(Parser, Clone, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Specify a test to run.
+    #[clap(long, help = "Run a specific derivation test fixture")]
+    pub test: Option<String>,
+    /// Runs all tests.
+    #[clap(long, help = "Run all derivation tests", conflicts_with = "test")]
+    pub all: bool,
+    /// Verbosity level (0-4)
+    #[arg(long, short, help = "Verbosity level (0-4)", action = ArgAction::Count)]
+    pub v: u8,
+}
+
+impl Cli {
+    /// Initializes telemtry for the application.
+    pub fn init_telemetry(self) -> Result<Self> {
+        let subscriber = tracing_subscriber::fmt()
+            .with_max_level(match self.v {
+                0 => Level::ERROR,
+                1 => Level::WARN,
+                2 => Level::INFO,
+                3 => Level::DEBUG,
+                _ => Level::TRACE,
+            })
+            .finish();
+        tracing::subscriber::set_global_default(subscriber).map_err(|e| anyhow!(e))?;
+        Ok(self)
+    }
+
+    /// Parse the CLI arguments and run the command
+    pub async fn run(&self) -> Result<()> {
+        let fixtures = self.get_fixtures()?;
+        for (name, fixture) in fixtures {
+            self.exec(name, fixture).await?;
+        }
+        Ok(())
+    }
+
+    /// Executes a given derivation test fixture.
+    pub async fn exec(&self, name: String, fixture: DerivationFixture) -> Result<()> {
+        info!(target: "exec", "Running test: {}", name);
+        let pipeline = crate::pipeline::new_runner_pipeline(fixture.clone()).await?;
+        match crate::runner::run(pipeline, fixture).await {
+            Ok(_) => {
+                println!("[PASS] {}", name);
+                Ok(())
+            }
+            Err(e) => {
+                error!(target: "exec", "[FAIL] {}", name);
+                Err(e)
+            }
+        }
+    }
+
+    /// Get [DerivationFixture]s to run.
+    pub fn get_fixtures(&self) -> Result<Vec<(String, DerivationFixture)>> {
+        // Get available derivation test fixtures
+        let available_tests = Self::get_tests()?;
+        trace!("Available tests: {:?}", available_tests);
+
+        // Parse derivation test fixtures
+        let tests = available_tests
+            .iter()
+            .map(|f| {
+                let path =
+                    f.path().to_str().ok_or_else(|| anyhow!("Failed to convert path to string"))?;
+                let fixture_str =
+                    f.contents_utf8().ok_or_else(|| anyhow!("Failed to read file contents"))?;
+                debug!("Parsing test fixture: {}", path);
+                Ok((
+                    path.to_string(),
+                    serde_json::from_str::<DerivationFixture>(fixture_str)
+                        .map_err(|e| anyhow!(e))?,
+                ))
+            })
+            .collect::<Result<Vec<_>>>()?;
+        trace!("Parsed {} tests", tests.len());
+
+        // Select the tests to run.
+        let fixtures = if self.all {
+            tests
+        } else if let Some(test) = &self.test {
+            let fixture = tests
+                .into_iter()
+                .find(|(path, _)| {
+                    std::path::Path::new(path)
+                        .file_name()
+                        .map(|f| {
+                            f.to_str().unwrap().strip_suffix(".json").unwrap_or(f.to_str().unwrap())
+                        })
+                        .map(|f| f.ends_with(&test.strip_suffix(".json").unwrap_or(test)))
+                        .unwrap_or(false)
+                })
+                .ok_or_else(|| anyhow!("Test not found"))?;
+            vec![fixture]
+        } else {
+            warn!("No test specified, running all tests");
+            tests
+        };
+        trace!("Selected {} tests", fixtures.len());
+
+        Ok(fixtures)
+    }
+
+    /// Get a list of available tests in the `op-test-vectors` git submodule.
+    pub fn get_tests() -> Result<Vec<File<'static>>> {
+        let mut tests = Vec::with_capacity(OP_TEST_VECTORS.entries().len());
+        for path in OP_TEST_VECTORS.entries() {
+            let DirEntry::File(f) = path else {
+                debug!(target: "get_tests", "Skipping non-file: {:?}", path);
+                continue;
+            };
+            tests.push(f.clone());
+        }
+        Ok(tests)
+    }
+}
---
+++ bin/kdn/src/providers.rs
@@ -0,0 +1,151 @@
+//! Fixture providers using data from the derivation test fixture.
+
+use alloy_consensus::{Header, Receipt, TxEnvelope};
+use alloy_eips::eip2718::Decodable2718;
+use alloy_primitives::B256;
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
+use op_test_vectors::{
+    derivation::DerivationFixture,
+    kona_derive::{
+        traits::{ChainProvider, L2ChainProvider},
+        types::{
+            BlockInfo, L2BlockInfo, L2ExecutionPayload, L2ExecutionPayloadEnvelope, RollupConfig,
+            SystemConfig,
+        },
+    },
+};
+use std::sync::Arc;
+
+/// An L1Provider using the fixture data from the derivation test fixture.
+#[derive(Debug, Clone)]
+pub struct FixtureL1Provider {
+    inner: DerivationFixture,
+}
+
+impl From<DerivationFixture> for FixtureL1Provider {
+    fn from(inner: DerivationFixture) -> Self {
+        Self { inner }
+    }
+}
+
+#[async_trait]
+impl ChainProvider for FixtureL1Provider {
+    async fn header_by_hash(&mut self, hash: B256) -> Result<Header> {
+        let Some(l1_block) = self.inner.l1_blocks.iter().find(|b| b.header.hash_slow() == hash)
+        else {
+            return Err(anyhow!("Block not found"));
+        };
+        Ok(l1_block.header.clone())
+    }
+
+    async fn block_info_by_number(&mut self, number: u64) -> Result<BlockInfo> {
+        let Some(l1_block) = self.inner.l1_blocks.iter().find(|b| b.header.number == number) else {
+            return Err(anyhow!("Block not found"));
+        };
+        Ok(BlockInfo {
+            number: l1_block.header.number,
+            hash: l1_block.header.hash_slow(),
+            parent_hash: l1_block.header.parent_hash,
+            timestamp: l1_block.header.timestamp,
+        })
+    }
+
+    async fn receipts_by_hash(&mut self, hash: B256) -> Result<Vec<Receipt>> {
+        let Some(l1_block) = self.inner.l1_blocks.iter().find(|b| b.header.hash_slow() == hash)
+        else {
+            return Err(anyhow!("Block not found"));
+        };
+        Ok(l1_block.receipts.clone())
+    }
+
+    async fn block_info_and_transactions_by_hash(
+        &mut self,
+        hash: B256,
+    ) -> Result<(BlockInfo, Vec<TxEnvelope>)> {
+        let Some(l1_block) = self.inner.l1_blocks.iter().find(|b| b.header.hash_slow() == hash)
+        else {
+            return Err(anyhow!("Block not found"));
+        };
+        let mut decoded = Vec::with_capacity(l1_block.transactions.len());
+        for tx in l1_block.transactions.iter() {
+            let decoded_tx = TxEnvelope::decode_2718(&mut &tx[..]).map_err(|e| anyhow!(e))?;
+            decoded.push(decoded_tx);
+        }
+        Ok((
+            BlockInfo {
+                number: l1_block.header.number,
+                hash: l1_block.header.hash_slow(),
+                parent_hash: l1_block.header.parent_hash,
+                timestamp: l1_block.header.timestamp,
+            },
+            decoded,
+        ))
+    }
+}
+
+/// An L2Provider using the fixture data from the derivation test fixture.
+#[derive(Debug, Clone)]
+pub struct FixtureL2Provider {
+    inner: DerivationFixture,
+}
+
+impl From<DerivationFixture> for FixtureL2Provider {
+    fn from(inner: DerivationFixture) -> Self {
+        Self { inner }
+    }
+}
+
+#[async_trait]
+impl L2ChainProvider for FixtureL2Provider {
+    async fn l2_block_info_by_number(&mut self, number: u64) -> Result<L2BlockInfo> {
+        self.inner.l2_block_infos.get(&number).cloned().ok_or_else(|| anyhow!("Block not found"))
+    }
+
+    async fn payload_by_number(&mut self, number: u64) -> Result<L2ExecutionPayloadEnvelope> {
+        let l2_block = self.l2_block_info_by_number(number).await?;
+        let mut payload = self.inner.l2_payloads.get(&number);
+        if payload.is_none() {
+            payload = self.inner.ref_payloads.get(&number);
+        }
+        let Some(payload) = payload else {
+            return Err(anyhow!("Payload not found"));
+        };
+        Ok(L2ExecutionPayloadEnvelope {
+            parent_beacon_block_root: payload.parent_beacon_block_root,
+            execution_payload: L2ExecutionPayload {
+                parent_hash: l2_block.block_info.parent_hash,
+                fee_recipient: payload.fee_recipient,
+                prev_randao: payload.prev_randao,
+                block_number: l2_block.block_info.number,
+                gas_limit: payload.gas_limit.unwrap_or_default() as u128,
+                timestamp: l2_block.block_info.timestamp,
+                block_hash: l2_block.block_info.hash,
+                transactions: payload.transactions.clone().into_iter().map(|tx| tx.0).collect(),
+                withdrawals: payload.withdrawals.clone(),
+                // These fields aren't used in derivation for span batch checking anyways.
+                state_root: Default::default(),
+                receipts_root: Default::default(),
+                logs_bloom: Default::default(),
+                gas_used: Default::default(),
+                extra_data: Default::default(),
+                base_fee_per_gas: Default::default(),
+                deserialized_transactions: Default::default(),
+                blob_gas_used: Default::default(),
+                excess_blob_gas: Default::default(),
+            },
+        })
+    }
+
+    async fn system_config_by_number(
+        &mut self,
+        number: u64,
+        _: Arc<RollupConfig>,
+    ) -> Result<SystemConfig> {
+        self.inner
+            .l2_system_configs
+            .get(&number)
+            .cloned()
+            .ok_or_else(|| anyhow!("System config not found"))
+    }
+}
