2fc3e2f633bcaffbc259ffbecafe64e786c0c376
refcell <abigger87@gmail.com>
2024-08-23 11:35:12 -0400

    fix(workspace): add unused dep lint

--- crates/derive/src/lib.rs
+++ crates/derive/src/lib.rs
@@ -2,8 +2,8 @@
 #![warn(missing_debug_implementations, missing_docs, unreachable_pub, rustdoc::all)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(not(any(test, feature = "metrics")), no_std)]
+#![cfg_attr(not(any(test, feature = "test-utils")), warn(unused_crate_dependencies))]
 
 extern crate alloc;
 
