b67d258a512c6aa9494ce91cc728298669298117
refcell <abigger87@gmail.com>
2024-09-23 11:15:12 -0400

    fix: use upstream provider types

--- crates/derive/src/online/alloy_providers.rs
+++ crates/derive/src/online/alloy_providers.rs
@@ -8,7 +8,7 @@ use alloy_provider::{Provider, ReqwestProvider};
 use alloy_rlp::{Buf, Decodable};
 use alloy_transport::{RpcError, TransportErrorKind, TransportResult};
 use async_trait::async_trait;
-use core::{num::NonZeroUsize, str::FromStr};
+use core::num::NonZeroUsize;
 use kona_primitives::{Block, L2ExecutionPayloadEnvelope, OpBlock};
 use lru::LruCache;
 use op_alloy_genesis::{RollupConfig, SystemConfig};
@@ -59,29 +59,13 @@ impl AlloyChainProvider {
     }
 
     /// Returns the latest L2 block number.
-    pub async fn latest_block_number(&mut self) -> Result<u64> {
-        let b: TransportResult<alloc::string::String> =
-            self.inner.raw_request("eth_blockNumber".into(), ()).await;
-        match b {
-            Ok(s) => {
-                let s = alloc::string::String::from(s.trim_start_matches("0x"));
-                u64::from_str_radix(&s, 16).map_err(|e| anyhow!(e))
-            }
-            Err(e) => Err(anyhow!(e)),
-        }
+    pub async fn latest_block_number(&mut self) -> Result<u64, RpcError<TransportErrorKind>> {
+        self.inner.get_block_number().await
     }
 
     /// Returns the chain ID.
     pub async fn chain_id(&mut self) -> Result<u64, RpcError<TransportErrorKind>> {
-        let chain_id: TransportResult<alloc::string::String> =
-            self.inner.raw_request("eth_chainId".into(), ()).await;
-        let chain_id = match chain_id {
-            Ok(s) => {
-                U64::from_str(s.as_str()).map_err(|e| RpcError::LocalUsageError(Box::new(e)))?
-            }
-            Err(e) => return Err(e),
-        };
-        Ok(chain_id.to::<u64>())
+        self.inner.get_chain_id().await
     }
 }
 
@@ -305,26 +289,12 @@ impl AlloyL2ChainProvider {
 
     /// Returns the chain ID.
     pub async fn chain_id(&mut self) -> Result<u64, RpcError<TransportErrorKind>> {
-        let chain_id: TransportResult<alloc::string::String> =
-            self.inner.raw_request("eth_chainId".into(), ()).await;
-        let chain_id = match chain_id {
-            Ok(s) => {
-                U64::from_str(s.as_str()).map_err(|e| RpcError::LocalUsageError(Box::new(e)))?
-            }
-            Err(e) => return Err(e),
-        };
-        Ok(chain_id.to::<u64>())
+        self.inner.get_chain_id().await
     }
 
     /// Returns the latest L2 block number.
     pub async fn latest_block_number(&mut self) -> Result<u64, RpcError<TransportErrorKind>> {
-        let s = self
-            .inner
-            .raw_request::<(), alloc::string::String>("eth_blockNumber".into(), ())
-            .await?;
-        U64::from_str(s.as_str())
-            .map_err(|e| RpcError::LocalUsageError(Box::new(e)))
-            .map(|u| u.to::<u64>())
+        self.inner.get_block_number().await
     }
 
     /// Creates a new [AlloyL2ChainProvider] from the provided [reqwest::Url].
