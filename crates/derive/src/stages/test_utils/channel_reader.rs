d7d2c1053b078a364f60748c1deea512cd831394
refcell <abigger87@gmail.com>
2024-10-08 16:59:28 -0400

    chore(derive): test channel reader resets (#660)

--- crates/derive/src/stages/test_utils/channel_reader.rs
+++ crates/derive/src/stages/test_utils/channel_reader.rs
@@ -18,12 +18,14 @@ pub struct MockChannelReaderProvider {
     pub data: Vec<PipelineResult<Option<Bytes>>>,
     /// The origin block info
     pub block_info: Option<BlockInfo>,
+    /// Tracks if the channel reader provider has been reset.
+    pub reset: bool,
 }
 
 impl MockChannelReaderProvider {
     /// Creates a new [MockChannelReaderProvider] with the given data.
     pub fn new(data: Vec<PipelineResult<Option<Bytes>>>) -> Self {
-        Self { data, block_info: Some(BlockInfo::default()) }
+        Self { data, block_info: Some(BlockInfo::default()), reset: false }
     }
 }
 
@@ -50,6 +52,7 @@ impl ChannelReaderProvider for MockChannelReaderProvider {
 #[async_trait]
 impl ResettableStage for MockChannelReaderProvider {
     async fn reset(&mut self, _base: BlockInfo, _cfg: &SystemConfig) -> PipelineResult<()> {
+        self.reset = true;
         Ok(())
     }
 }
