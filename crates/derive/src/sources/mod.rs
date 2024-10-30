7ee4b2e5c961aca2607ec0f7edbbcf775cc64c24
refcell <abigger87@gmail.com>
2024-10-30 09:46:01 -0400

    feat(derive): sources docs

--- crates/derive/src/sources/mod.rs
+++ crates/derive/src/sources/mod.rs
@@ -1,4 +1,11 @@
-//! This module contains data source impelmentations.
+//! The data source module.
+//!
+//! Data sources are data providers for the kona derivation pipeline.
+//! They implement the [DataAvailabilityProvider] trait, providing a way
+//! to iterate over data for a given (L2) [BlockInfo].
+//!
+//! [DataAvailabilityProvider]: crate::traits::DataAvailabilityProvider
+//! [BlockInfo]: op_alloy_protocol::BlockInfo
 
 mod ethereum;
 pub use ethereum::EthereumDataSource;
