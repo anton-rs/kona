0e68dcdfb76511a367dd87dd0dbdacc00814d457
refcell <abigger87@gmail.com>
2024-10-30 16:14:23 -0400

    fix

--- crates/derive/src/attributes/mod.rs
+++ crates/derive/src/attributes/mod.rs
@@ -1,4 +1,6 @@
-//! Module containing [AttributesBuilder] trait implementations.
+//! Module containing the [AttributesBuilder] trait implementations.
+//!
+//! [AttributesBuilder]: crate::traits::AttributesBuilder
 
 mod stateful;
 pub use stateful::StatefulAttributesBuilder;
