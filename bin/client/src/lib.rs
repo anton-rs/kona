00eb8ce3b22f1d6f44a793ec26991bcacc5480fa
refcell <abigger87@gmail.com>
2024-12-04 13:30:06 -0500

    fix: lint

--- bin/client/src/lib.rs
+++ bin/client/src/lib.rs
@@ -2,6 +2,7 @@
 #![warn(missing_debug_implementations, missing_docs, unreachable_pub, rustdoc::all)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![allow(clippy::type_complexity)]
 #![no_std]
 
 extern crate alloc;
