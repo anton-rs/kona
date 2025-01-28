46d4a1e147ddab2d0adf8116ce50034bbd354c46
refcell <abigger87@gmail.com>
2025-01-28 14:13:26 -0500

    feat(executor): EIP-7002 Syscall Support [ISTHMUS] (#965)

--- crates/executor/src/executor/mod.rs
+++ crates/executor/src/executor/mod.rs
@@ -6,7 +6,7 @@ use crate::{
     errors::TrieDBError,
     syscalls::{
         ensure_create2_deployer_canyon, pre_block_beacon_root_contract_call,
-        pre_block_block_hash_contract_call,
+        pre_block_block_hash_contract_call, pre_block_withdrawals_request_contract_call,
     },
     ExecutorError, ExecutorResult, TrieDBProvider,
 };
@@ -146,6 +146,15 @@ where
             &initialized_cfg,
             &initialized_block_env,
             parent_block_hash,
+            &payload,
+        )?;
+
+        // Apply the pre-block EIP-7002 contract call.
+        pre_block_withdrawals_request_contract_call(
+            &mut state,
+            self.config,
+            &initialized_cfg,
+            &initialized_block_env,
             &payload,
         )?;
 
--- crates/executor/src/syscalls/mod.rs
+++ crates/executor/src/syscalls/mod.rs
@@ -3,6 +3,9 @@
 mod eip2935;
 pub(crate) use eip2935::pre_block_block_hash_contract_call;
 
+mod eip7002;
+pub(crate) use eip7002::pre_block_withdrawals_request_contract_call;
+
 mod eip4788;
 pub(crate) use eip4788::pre_block_beacon_root_contract_call;
 
